505c968cf40fdf815df121fd41080e6d3fd79dd8
Saravanan Sonia <srsonia98@yahoo.com.sg>
2020-02-29 22:36:25 -0800

    Button Click to Login Screen

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.helloworld.ui.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
@@ -19,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                openActivity2();
+                openLoginActivity();
             }
         });
     }
 
-    public void openActivity2(){
-        Intent intent = new Intent(this, Activity2.class);
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
